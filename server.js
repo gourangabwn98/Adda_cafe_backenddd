959129e2a159d3e781e7fedfe3eebfb594ed01ef
Gouranga Ghosh <88451025+gourangabwn98@users.noreply.github.com>
2026-03-05 04:34:29 +0530

    add in origin

--- server.js
+++ server.js
@@ -12,12 +12,25 @@ import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 connectDB();
 
 const app = express();
+
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 const allowedOrigins = [
   "http://localhost:5173",
   "https://adda-cafe-frontenddd.vercel.app",
 ];
 
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
