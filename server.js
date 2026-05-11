5fbc995d1f6931c0938f8667af29c8be867ec8be
Gouranga Ghosh <88451025+gourangabwn98@users.noreply.github.com>
2026-05-12 04:21:21 +0530

    add patch

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST","PATCH" ],
   },
 });
 
